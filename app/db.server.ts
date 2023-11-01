bcb68351eb5d639f8c967bf9d6a3417fbad8d012
freds-dev <frebruc@gmail.com>
2023-11-01 13:19:20 +0100

    set `POSTGRES_PRISMA_URL`

--- app/db.server.ts
+++ app/db.server.ts
@@ -7,10 +7,10 @@ import { singleton } from "./singleton.server";
 const prisma = singleton("prisma", getPrismaClient);
 
 function getPrismaClient() {
-  const { POSTGRES_URL } = process.env;
-  invariant(typeof POSTGRES_URL === "string", "POSTGRES_URL env var not set");
+  const { POSTGRES_PRISMA_URL } = process.env;
+  invariant(typeof POSTGRES_PRISMA_URL === "string", "POSTGRES_PRISMA_URL env var not set");
 
-  const databaseUrl = new URL(POSTGRES_URL);
+  const databaseUrl = new URL(POSTGRES_PRISMA_URL);
 
   const isLocalHost = databaseUrl.hostname === "localhost";
 
