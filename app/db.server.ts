6a4266b56214e22f08077c50eb2e1e69560b9b4e
freds-dev <frebruc@gmail.com>
2023-11-01 12:23:56 +0100

    setup postgres

--- app/db.server.ts
+++ app/db.server.ts
@@ -7,10 +7,10 @@ import { singleton } from "./singleton.server";
 const prisma = singleton("prisma", getPrismaClient);
 
 function getPrismaClient() {
-  const { DATABASE_URL } = process.env;
-  invariant(typeof DATABASE_URL === "string", "DATABASE_URL env var not set");
+  const { POSTGRES_URL } = process.env;
+  invariant(typeof POSTGRES_URL === "string", "POSTGRES_URL env var not set");
 
-  const databaseUrl = new URL(DATABASE_URL);
+  const databaseUrl = new URL(POSTGRES_URL);
 
   const isLocalHost = databaseUrl.hostname === "localhost";
 
