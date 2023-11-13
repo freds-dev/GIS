584086c89c056ad660e5a78c264617839483f58f
freds-dev <frebruc@gmail.com>
2023-11-13 12:13:20 +0100

    fix eslint

--- @/components/ui/dropdown-menu.tsx
+++ @/components/ui/dropdown-menu.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable react/prop-types */
 "use client"
 
-import * as React from "react"
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
 import { Check, ChevronRight, Circle } from "lucide-react"
+import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
