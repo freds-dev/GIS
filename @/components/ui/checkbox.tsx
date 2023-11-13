66f66f1f445eedcc78da5d993c91602481d43889
freds-dev <frebruc@gmail.com>
2023-11-13 13:39:19 +0100

    fix ESLint

--- @/components/ui/checkbox.tsx
+++ @/components/ui/checkbox.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable react/prop-types */
 "use client"
 
-import * as React from "react"
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
 import { Check } from "lucide-react"
+import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
