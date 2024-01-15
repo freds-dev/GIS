2fdecfeab7c2d4919d9d1c8d68c62da379aec4ae
freds-dev <f_bruc03@uni-muenster.de>
2024-01-15 10:12:56 +0100

    add heatmap to admin navigation

--- app/components/dashboard/Navigation.tsx
+++ app/components/dashboard/Navigation.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NavLink } from "@remix-run/react";
-import { Flag, Gauge, Globe, TableProperties } from "lucide-react";
+import { Flag, Flame, Gauge, Globe, TableProperties } from "lucide-react";
 
 export default function Navigation(props: any) {
   return (
@@ -22,19 +22,34 @@ export default function Navigation(props: any) {
         </NavLink>
         {/* <!-- Profile --> */}
         {props.user.role === "ADMIN" ? (
-          <NavLink
-            to="/dashboard"
-            end
-            className={({ isActive, isPending }) =>
-              isPending
-                ? "h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white"
-                : isActive
-                ? "h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear text-gray-800 bg-white"
-                : "h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white"
-            }
-          >
-            <Gauge />
-          </NavLink>
+          <>
+            <NavLink
+              to="/heatmap"
+              end
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white"
+                  : isActive
+                  ? "h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear text-gray-800 bg-white"
+                  : "h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white"
+              }
+            >
+              <Flame />
+            </NavLink>
+            <NavLink
+              to="/dashboard"
+              end
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white"
+                  : isActive
+                  ? "h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear text-gray-800 bg-white"
+                  : "h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white"
+              }
+            >
+              <Gauge />
+            </NavLink>
+          </>
         ) : null}
 
         {/* <!-- Playgrounds --> */}
